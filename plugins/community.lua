5528a0b9cbd0d6a80ba5904330cc3a954cef574a
trinophite <trinophite@pm.me>
2024-02-15 19:08:45 -0500

    update config

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,14 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.bash" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
